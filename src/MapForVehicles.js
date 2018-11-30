46c3761c206f9b50e418661cc1d2efe4e6c01ea2
Thomas Chaigneau <thomas.a.chaigneau@gmail.com>
2018-11-30 16:43:55 +0100

    trajectories in meters per second and replace componentWillMount for componentDidMount

--- src/MapForVehicles.js
+++ src/MapForVehicles.js
@@ -8,42 +8,49 @@ class MapForVehicles extends Component {
     super(props);
     this.state = {
       currentTrajectorieNb: 0,
-      position: props.trajectoriesRefine[0],
+      position: props.trajectories[0],
+      // distance: props.distanceIntervals[0],
       busOnTheWay: false,
     };
     MapForVehicles.propTypes = {
-      trajectoriesRefine: PropTypes.instanceOf(Array).isRequired,
-      timeIntervals: PropTypes.instanceOf(Array).isRequired,
+      trajectories: PropTypes.instanceOf(Array).isRequired,
     };
   }
 
-
-  startVehiculesMove = (time) => {
+  startVehiculesMove = () => {
     this.setState({ busOnTheWay: true });
-    // Since the position is not regular in time,
-    // we move to the next coordinate after a variable time interval
-    setTimeout(() => {
-      const { trajectoriesRefine, timeIntervals } = this.props;
+    setInterval(() => {
+      const { trajectories } = this.props;
       const { currentTrajectorieNb } = this.state;
-      if (currentTrajectorieNb < trajectoriesRefine.length) {
+      if (currentTrajectorieNb < trajectories.length) {
         this.setState({
           currentTrajectorieNb: currentTrajectorieNb + 1,
-          position: trajectoriesRefine[currentTrajectorieNb],
-        }, this.startVehiculesMove(timeIntervals[currentTrajectorieNb]));
+          position: trajectories[currentTrajectorieNb],
+        });
       }
-    }, time * 10);
+    }, 10);
   };
 
   render() {
     const { position, busOnTheWay } = this.state;
-    const { timeIntervals } = this.props;
     const casablanca = [33.57311, -7.589843];
+    console.log('position', position);
+    if (!position) {
+      return (
+        <div>
+          <button onClick={() => this.startVehiculesMove()} type="button">
+              View Trajectory
+          </button>
+          <p>Loading</p>
+        </div>
+      );
+    }
     return (
       <div>
-        <button onClick={() => this.startVehiculesMove(timeIntervals[0])} type="button">
+        <button onClick={() => this.startVehiculesMove()} type="button">
           View Trajectory
         </button>
-        <Map className="Map" center={casablanca} zoom={12}>
+        <Map className="Map" center={casablanca} zoom={15}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png"
--- src/TrajectoryProjection.js
+++ src/TrajectoryProjection.js
@@ -4,62 +4,26 @@ import file from './data.json';
 
 const VehicleInfo = file.data[1];
 
-/**
- * distance between two GPS points
- * taking into account that the Earth is not flat.
- */
-const getDistance = (lat1, lon1, lat2, lon2) => {
-  const degToRad = deg => deg * (Math.PI / 180);
-  const R = 6371 * 1000; // Radius of the earth in meters
-  const dLat = degToRad(lat2 - lat1);
-  const dLon = degToRad(lon2 - lon1);
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
-  + Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2))
-  * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = R * c; // Distance in meters
-  return distance;
-};
-
 class TrajectoryProjection extends Component {
   state = {
-    trajectoriesRefine: [],
-    timeIntervals: [],
-    distanceIntervals: [],
+    trajectories: [],
   };
 
-  componentWillMount() {
-    const { trajectoriesRefine, timeIntervals, distanceIntervals } = this.state;
+  componentDidMount() {
+    const { trajectories } = this.state;
     VehicleInfo.trajectories.forEach((roadPortion) => {
       const LatDif = roadPortion.end_lat - roadPortion.start_lat;
       const LonDif = roadPortion.end_lon - roadPortion.start_lon;
-      const distanceInterval = getDistance(
-        roadPortion.start_lon,
-        roadPortion.start_lat,
-        roadPortion.end_lon,
-        roadPortion.end_lat,
-      );
+      const timeInterval = (roadPortion.end_datetime - roadPortion.start_datetime);
 
-      // we cut each roadPortion lines into smaller ones.
-      // The number of smaller portions depends of the line size.
-      // we adapt the timelapse to these new portions
-      const distanceDivider = Math.round(distanceInterval / 10);
-      for (let j = 0; j < distanceDivider; j += 1) {
-        const latRefine = roadPortion.start_lat + (j) * LatDif / distanceDivider;
-        const lonRefine = roadPortion.start_lon + (j) * LonDif / distanceDivider;
-        const timeInterval = (roadPortion.end_datetime - roadPortion.start_datetime)
-                             / distanceDivider;
-
-        trajectoriesRefine.push([latRefine, lonRefine]);
-        timeIntervals.push(timeInterval);
-        distanceIntervals.push(distanceInterval);
+      for (let j = 0; j < timeInterval; j += 1) {
+        const latRefine = roadPortion.start_lat + (j) * LatDif / timeInterval;
+        const lonRefine = roadPortion.start_lon + (j) * LonDif / timeInterval;
+        trajectories.push([latRefine, lonRefine]);
       }
     });
-
     this.setState({
-      trajectoriesRefine,
-      timeIntervals,
-      distanceIntervals,
+      trajectories,
     });
   }
 
@@ -67,7 +31,7 @@ class TrajectoryProjection extends Component {
     return (
       <div>
         <ul>
-          <span>Your are looking for vehicle number:</span>
+          <span>Your are looking for vehiclue number:</span>
           <li>{VehicleInfo.id}</li>
           <span>Depature date:</span>
           <li>{VehicleInfo.start_date}</li>
